3ffe812b8665e5f7b5cac058da7fc7bddc19fbe7
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-12-16 21:36:17 +0100

    temlaptes&forms&auth

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+
+def signup(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
+    context = {'form':form}
+    return render(request, 'blog/signup.html', context=context)
+
+
+def login(request):
+    form = LoginForm()
+
+    if request.method == "POST":
+
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                return redirect("home")
+
+    context = {'form':form}
+
+    return render(request, 'blog/login.html', context=context)
+
+def posts(request):
+    return render(request, 'blog/posts.html')

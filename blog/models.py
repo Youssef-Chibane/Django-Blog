ea78f515d1eac445b32c9773abd60896c981369b
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-12-19 13:06:45 +0100

    Post Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    cover = models.TextField()
+    body = models.TextField()
+    published_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')  # User who published the post
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title

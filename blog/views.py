296753efee063d7be978d6433dbfdd4493b06423
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-12-20 17:02:26 +0100

    all posts pagination

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def home(request):
@@ -60,7 +61,10 @@ def logout(request):
 
 @login_required(login_url='login')
 def posts(request):
-    posts = Post.objects.all().order_by('-created_at')
+    posts_list = Post.objects.all().order_by('-created_at')
+    paginator = Paginator(posts_list, 9)  # Show 9 posts per page
+    page_number = request.GET.get('page')  # Get the page number from the query parameter
+    posts = paginator.get_page(page_number)  # Get the posts for the current page
     context = {'posts': posts}
     return render(request, 'blog/posts.html', context=context)
 

728fa9c919054c2f35768f0c8194d45470ca29bd
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-12-22 20:06:35 +0100

    new urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name="logout"),
     path('posts', views.posts, name='posts'),
-    path('post_detail/<int:pk>', views.post_detail, name='post_detail'),
-    path('create_post', views.create_post, name='create_post'),
-    path('update_post/<int:pk>', views.update_post, name='update_post'),
+    path('post/create', views.create_post, name='create'),
+    path('post/<int:pk>/detail', views.post_detail, name='detail'),
+    path('post/<int:pk>/update', views.update_post, name='update'),
+    path('post/<int:pk>/delete', views.delete_post, name='delete'),
 ]

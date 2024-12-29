9d3740f1ec9dcdbe9a5d87c5be2358a3b3418c81
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-12-29 14:52:16 +0100

    about&contact

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
     path('signup', views.signup, name='signup'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name="logout"),
@@ -13,8 +15,6 @@ urlpatterns = [
     path('post/<int:pk>/detail', views.post_detail, name='detail'),
     path('post/<int:pk>/update', views.update_post, name='update'),
     path('post/<int:pk>/delete', views.delete_post, name='delete'),
-    path('contact', views.contact, name='contact'),
-    path('about', views.about, name='about'),
 ]
 
 
